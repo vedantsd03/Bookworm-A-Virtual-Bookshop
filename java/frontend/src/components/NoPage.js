d33c2fa8eabb3e120b645f3598ba78c1169a42fa
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2024-03-05 17:05:44 +0530

    Delete java directory

--- java/frontend/src/components/NoPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NoPage() {
-  return (
-    <div>404 - Page Not Found</div>
-  )
-}
--- java/frontend/src/components/ShowPDF.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ShowPDF({filename}) {
-    const imageUrl = process.env.PUBLIC_URL + '/book_pdfs/'+filename+'.pdf'; 
-
-  return (
-    <img src={imageUrl} alt="My Image" />
-    )
-}
